58dd54300418170fc566aa61af07a78ffc9778a6
Brigitte <114067290+brishar0n@users.noreply.github.com>
2022-12-19 22:58:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Please put your answer here
+
+$(function(){
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+  
+  $('.close-modal').click(function(){
+    $('#signup-modal').fadeOut()
+  });
+
+});
